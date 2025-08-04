4eecc6056309bd5573f864b7846eecd2960ebed5
oosterwaal <hhp.oosterwaal@gmail.com>
2025-08-04 11:17:50 +0200

    movement

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
     
     clock = pygame.time.Clock()
     dt = 0
-    from player import Player
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
--- player.py
+++ player.py
@@ -1,14 +1,30 @@
-# ExamplePlayer class implementation
-class Player(SomeParentClass):
+import pygame
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+
+class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation = 0 
+        super().__init__()
+        self.image = pygame.Surface((PLAYER_RADIUS * 2, PLAYER_RADIUS * 2), pygame.SRCALPHA)
+        pygame.draw.polygon(self.image, "white", [(PLAYER_RADIUS, 0), (0, PLAYER_RADIUS), (PLAYER_RADIUS, PLAYER_RADIUS * 2)], 2)
+        self.rect = self.image.get_rect(center=(x, y))
+        self.rotation = 0
     def triangle(self):
-    forward = pygame.Vector2(0, 1).rotate(self.rotation)
-    right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-    a = self.position + forward * self.radius
-    b = self.position - forward * self.radius - right
-    c = self.position - forward * self.radius + right
-    return [a, b, c]
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * PLAYER_RADIUS / 1.5
+        center = pygame.Vector2(self.rect.center)
+        a = center + forward * PLAYER_RADIUS
+        b = center - forward * PLAYER_RADIUS - right
+        c = center - forward * PLAYER_RADIUS + right
+        return [a, b, c]
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
