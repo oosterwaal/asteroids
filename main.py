1dc50187e362f0e309189e76ec08903afdce059d
oosterwaal <hhp.oosterwaal@gmail.com>
2025-08-04 10:59:18 +0200

    main adjusting

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
+    
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +17,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
