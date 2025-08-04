dded7c9e1d9e1e9665160958b6381eff4fb9c405
oosterwaal <hhp.oosterwaal@gmail.com>
2025-08-04 10:55:07 +0200

    making it better

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
 
-if __name__ == "__main__":
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    main()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        pygame.display.flip()           
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
